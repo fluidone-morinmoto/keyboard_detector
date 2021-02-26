6a60e68cdb2931dcf2da99d64442223a43c3731c
Fluidone Morinmoto <morin@fluidone.it>
2021-02-26 18:36:06 +0100

    Closes #688 @0.5 Implemented a simple configuration for only play sounds on the specified keyboard

--- detector.py
+++ detector.py
@@ -1,6 +1,5 @@
 import keyboard
-
-device = '/dev/input/event13'
+import json
 
 codes = [
     29,  #left control
@@ -13,11 +12,38 @@ codes = [
     69   #num lock
 ]
 
+device = None
+config = None
+available_keys = []
+mapped_sounds = {}
+
+with open('./config.json') as file:
+    config = json.load(file)
+    mapped_sounds = config["mapped_sounds"]
+    device = config["device"]
+    # print("-------------------------")
+    # print(mapped_sounds)
+    # print("-------------------------")
+    # print(available_keys)
+    # print("-------------------------")
+
+## Dedicated function for play sounds
+def _play_sound(sound):
+    msg = "Ready to play {}"
+    print(msg.format(sound))
+
+## Detects if the pressed key come from the right device and is related to a
+## command
 def _detect_key(event):
     if event.device == device:
-        if event.scan_code in codes:
-            msg = "Pressed '{}' with keycode {} on ther right keyboard"
-            print(msg.format(event.name, event.scan_code))
+        scan_code = "{}".format(event.scan_code)
+        if scan_code in mapped_sounds.keys():
+            msg = "Pressed {} and play a sound".format(scan_code)
+            print(msg)
+            _play_sound(mapped_sounds[scan_code])
+        else:
+            print("{} is not in keys ".format(event.scan_code))
+            print(mapped_sounds.keys())
 
 ## Hook the function for key detection on key release
 keyboard.on_release(_detect_key)
